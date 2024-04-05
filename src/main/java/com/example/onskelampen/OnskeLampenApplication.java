3c78499e73fed25ec5bfef5be47e7468644f4f76
Oskar <osth0002@stud.kea.dk>
2024-04-05 14:33:27 +0200

    fix

---
+++ src/main/java/com/example/onskelampen/OnskeLampenApplication.java
@@ -0,0 +1,13 @@
+package com.example.onskelampen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnskeLampenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnskeLampenApplication.class, args);
+    }
+
+}
