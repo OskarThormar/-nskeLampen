df2e1933ff3d668c25bf3b51d2030f60a6d97fce
Oskar <osth0002@stud.kea.dk>
2024-04-18 16:08:28 +0200

    Changes by Jesper

--- src/main/java/com/example/onskelampen/controller/OnskeLampenController.java
+++ src/main/java/com/example/onskelampen/controller/OnskeLampenController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ServerErrorException;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
