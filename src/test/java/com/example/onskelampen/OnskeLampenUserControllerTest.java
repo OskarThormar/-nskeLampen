534f7f4174541c3d0859ffc4be6da099b5df0a26
Oskar <osth0002@stud.kea.dk>
2024-04-18 14:54:59 +0200

    Changes by Oskar

--- src/test/java/com/example/onskelampen/OnskeLampenUserControllerTest.java
+++ src/test/java/com/example/onskelampen/OnskeLampenUserControllerTest.java
@@ -25,43 +25,4 @@ class OnskeLampenUserControllerTest {
 
     private MockMvc mockMvc;
 
-    @Test
-    void testLandingPage() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-        mockMvc.perform(MockMvcRequestBuilders.get("/user"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("User-LandingPage"));
-    }
-
-    @Test
-    void testRegister() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-        mockMvc.perform(MockMvcRequestBuilders.get("/user/register"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("register"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("userForm"));
-    }
-
-    @Test
-    void testRegisterUser_Success() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-        User user = new User();
-        when(userService.register(user)).thenReturn(true);
-        mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
-                        .flashAttr("userForm", user))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/user/login"));
-    }
-
-    @Test
-    void testRegisterUser_Failure() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-        User user = new User();
-        when(userService.register(user)).thenReturn(false);
-        mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
-                        .flashAttr("userForm", user))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("register"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("error"));
-    }
 }
